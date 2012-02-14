00da8d0f7e379c30ee952a3a25c9f28900c99bfc
Finlay Thompson <finlay@dragonfly.co.nz>
2012-02-15 10:31:36 +1300

    Updated to add a close in version

--- experiment/differences.r
+++ experiment/differences.r
@@ -1,6 +1,5 @@
 
 d <- read.csv('differences.csv')
-d <- d[seq(1,2000,1), ]
 
 svg(file='differences.svg', width=10, height=7)
 
@@ -8,6 +7,26 @@ plot(d$x,  d$lgd,   type='l', log='xy', axes=FALSE, xlab='', ylab='')
 lines(d$x, d$lgfnd, type='l', col='blue')
 lines(d$x, d$lgld,  type='l', col='red')
 lines(d$x, d$lgd,   type='l', col='black')
+
+axis(1)
+yticks <- axTicks(2)
+axis(2, las=1, at=yticks, label=yticks)
+legend('topright', c('logGamma', 'logGammaL', 'logGammaFN'),
+       col=c('black','red', 'blue'),
+       pch=16,
+       bty='n')
+
+dev.off()
+
+svg(file='differences-close.svg', width=10, height=7)
+
+plot(d$x,  d$lgd,   type='l', log='xy', axes=FALSE, 
+    xlab='', ylab='', xlim=c(1e-2,1e2))
+lines(d$x, d$lgfnd, type='l', col='blue')
+lines(d$x, d$lgld,  type='l', col='red')
+lines(d$x, d$lgd,   type='l', col='black')
+
+abline(v=c(1,2))
 
 axis(1)
 yticks <- axTicks(2)
